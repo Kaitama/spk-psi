39fd1242489a3a487bd9278ffc2031456d6c2be8
Khairi Ibnutama <mr.ibnutama@gmail.com>
2023-02-05 14:38:36 +0700

    Finishing PSI Method

---
+++ app/Http/Livewire/Proses/Index.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Livewire\Proses;
+
+use App\Models\Alternatif;
+use App\Models\Kriteria;
+use Livewire\Component;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+class Index extends Component
+{
+	public function render()
+	{
+		$alternatifs = $this->proses();
+		return view('livewire.proses.index', compact('alternatifs'));
+	}
+
+	public function print()
+	{
+		// abaikan garis error di bawah 'Pdf' jika ada.
+		$pdf = Pdf::loadView('laporan.cetak', ['data' => $this->proses()])->output();
+		// return $pdf->download('Laporan.pdf');
+		return response()->streamDownload(fn () => print($pdf), 'Laporan.pdf');
+	}
+
+	// proses metode PSI
+	public function proses()
+	{
+		$alternatifs = Alternatif::orderBy('kode')->get();
+		$kriterias = Kriteria::orderBy('kode')->get('type')->toArray();
+		// dd($kriterias);
+
+		// penentuan matriks keputusan
+		$Xij = [];
+		foreach ($alternatifs as $ka => $alt) {
+			foreach ($alt->kriteria as $kk => $krit) {
+				$Xij[$ka][$kk] = $krit->pivot->nilai;
+			}
+		}
+
+		// normalisasi matriks keputusan
+		$rows = count($Xij);
+		$cols = count($Xij[0]);
+		$Nij = [];
+		for ($j = 0; $j < $cols; $j++) {
+			$xj = [];
+			for ($i = 0; $i < $rows; $i++) {
+				$xj[] = $Xij[$i][$j];
+			}
+
+			$divisor = max($xj);
+			$cost = false;
+			if ($kriterias[$j]['type'] == false) {
+				$cost = true;
+				$divisor = min($xj);
+			}
+
+			foreach ($xj as $kj => $x) {
+				$Nij[$kj][$j] = $cost ? ($divisor / $x) : ($x / $divisor);
+			}
+		}
+
+		// menjumlahkan elemen tiap kolom matriks
+		$EN = [];
+		for ($i = 0; $i < $cols; $i++) {
+			$jumlah = 0;
+			for ($j = 0; $j < $rows; $j++) {
+				$jumlah += $Nij[$j][$i];
+			}
+			$EN[] = $jumlah;
+		}
+
+		// hitung nilai mean
+		$N = [];
+		foreach ($EN as $e) {
+			$N[] = $e / $rows;
+		}
+
+		// hitung variasi preferensi
+		$Tj = [];
+		for ($i = 0; $i < $cols; $i++) {
+			for ($j = 0; $j < $rows; $j++) {
+				$Tj[$j][$i] = pow($Nij[$j][$i] - $N[$i], 2);
+			}
+		}
+
+		// hitung total tiap kriteria
+		$TTj = [];
+		for ($i = 0; $i < $cols; $i++) {
+			$jumlah = 0;
+			for ($j = 0; $j < $rows; $j++) {
+				$jumlah += $Tj[$j][$i];
+			}
+			$TTj[] = $jumlah;
+		}
+
+		// menentukan penyimpangan nilai preferensi
+		$Omega = [];
+		foreach ($TTj as  $ttj) {
+			$Omega[] = 1 - $ttj;
+		}
+
+		// total penyimpangan nilai preferensi
+		$EOmega = array_sum($Omega);
+
+		// menghitung kriteria bobot
+		$Wj = [];
+		foreach ($Omega as $o) {
+			$Wj[] = round($o / $EOmega, 4);
+		}
+
+		// menghitung PSI
+		$ThetaI = [];
+		for ($i = 0; $i < $cols; $i++) {
+			for ($j = 0; $j < $rows; $j++) {
+				$ThetaI[$j][$i] = round($Nij[$j][$i] * $Wj[$i], 4);
+			}
+		}
+
+		// penjumlahan tiap baris proses sebelumnya
+		$TThetaI = [];
+		foreach ($ThetaI as $theta) {
+			$TThetaI[] = array_sum($theta);
+		}
+
+		foreach ($alternatifs as $key => $alternatif) {
+			$alternatif->nilai = $TThetaI[$key];
+		}
+
+		return $alternatifs;
+	}
+}
--- resources/views/livewire/proses/index.blade.php
+++ resources/views/livewire/proses/index.blade.php
@@ -1,12 +1,15 @@
-<div>
+<div id="ranking">
 	
+	@if ($alternatifs->count() > 0)
+		
 	{{-- tabel data alternatif --}}
 	<div class="container mx-auto px-4 sm:px-8">
 		<div class="py-8">
 			<div class="flex items-center justify-between">
-				<h2 class="text-2xl font-semibold leading-tight">Data Alternatif</h2>
-				<x-button-link href="{{ route('alternatif.create') }}">Tambah Alternatif</x-button-link>
+				<h2 class="text-2xl font-semibold leading-tight">Hasil Perankingan</h2>
+				<x-jet-button class="button" wire:click="print">Cetak</x-jet-button>
 			</div>
+			<p>Data berikut diurutkan berdasarkan nilai tertinggi.</p>
 			<div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
 				<div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
 					<table class="min-w-full leading-normal">
@@ -16,20 +19,23 @@
 									No
 								</th>
 								<th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-900 text-white text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-									Kode Alternatif
+									Kode
 								</th>
 								<th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-900 text-white text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-									Nama Alternatif
+									Nama
 								</th>
-								<th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-900 text-white text-center text-xs font-semibold text-gray-600 uppercase tracking-wider"></th>
+								<th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-900 text-white text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+									Nilai
+								</th>
 							</tr>
 						</thead>
 						<tbody>
-							@forelse ($alternatifs as $index => $alt)
-									
+							@php $no = 1 @endphp
+							@forelse ($alternatifs->sortByDesc('nilai') as $index => $alt)
+							
 							<tr>
 								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-									{{ $index + 1 }}
+									{{ $no++ }}
 								</td>
 								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
 									{{ $alt->kode }}
@@ -38,21 +44,17 @@
 									{{ $alt->name }}
 								</td>
 								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-									<div class="flex items-center justify-end gap-4">
-										<a href="{{ route('alternatif.edit', $alt->id) }}" class="uppercase font-medium text-xs text-gray-700">Ubah</a>
-										<x-jet-button wire:click="delete({{ $alt->id }})">Hapus</x-jet-button>
-									</div>
+									{{ $alt->nilai }}
 								</td>
+								
 							</tr>
 
 							@empty
 
 							<tr>
-								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm" colspan="6">
-									Data alternatif masih kosong.
-								</td>
+								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm" colspan="{{ $kriterias->count() + 4 }}">Belum ada data alternatif.</td>
 							</tr>
-
+							
 							@endforelse
 						</tbody>
 					</table>
@@ -60,4 +62,6 @@
 			</div>
 		</div>
 	</div>
+
+	@endif
 </div>
